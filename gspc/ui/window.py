duttong/gspc_v2

commit 9706b03caa9ed28abdccaf68d37abe27ecf85750

Fix remove task index
Indices where changed to integers from QModelIndex, but the removal
handling missed this.

gspc/ui/window.py
@@ -644,10 +644,7 @@ class Main(QtWidgets.QMainWindow):
 
         def remove_task():
             index = selected_index()
-            if not index.isValid():
-                return
-            index = index.row()
-            if index < 0:
+            if index <= 0:
                 return
 
             item = task_list.takeItem(index)

commit 5331578efb0347c8c4986cde3fcd01bfcedb4872

Another fix to removal
Check index less than zero, otherwise we can't remove the first task.

Fixes: 9706b03caa9ed28abdccaf68d37abe27ecf85750

gspc/ui/window.py
@@ -644,7 +644,7 @@ class Main(QtWidgets.QMainWindow):
 
         def remove_task():
             index = selected_index()
-            if index <= 0:
+            if index < 0:
                 return
 
             item = task_list.takeItem(index)